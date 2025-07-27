c33b2d14c0df427bded386de2f038d1314ef30fa
sargones <pipisargones@gmail.com>
2025-07-27 18:00:28 +0200

    1

---
+++ Home.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas as pd
+# from main import artist_df
+
+st.set_page_config(
+    page_title='Interactive Podcast with Renate', page_icon='👋')  # 🐫
+
+st.title("🎙️'Interactive Podcast'🎙️")
+st.subheader('with Renate and Plamen')
+spaces = ("""
+
+
+
+
+
+        """)
+
+
+st.text(spaces)
+
+st.image('podcast_home.png')
+st.text(spaces)
+
+st.text(""" 
+        Welcome to "Interactive Podcast" a podcast where two individuals, brought together by the serendipity of a dating app, explore the intricate tapestry of life from their unique perspectives. I am Plamen, and joining me is Renate, and together, we dive deep into the multifaceted aspects of our lives, from the professional to the personal, and everything in between. 
+       
+         Each call/chat, we navigate the winding paths of our respective journeys, sharing stories, insights, and sometimes even a few laughs. Whether it is the challenges we face in our careers, the joys and hurdles of our relationships, or the unexpected twists and turns that life throws our way, we are here to unpack it all with honesty and vulnerability.
+
+        From the boardroom to the living room, from the highs of success to the lows of failure, we believe that every story has a lesson to offer. Our backgrounds may be different, but our shared human experience creates a rich and relatable dialogue. So, whether you are looking for a dose of inspiration, a bit of advice, or just a good conversation, you have come to the right place.
+
+        Join us as we embark on this journey of discovery and connection, and perhaps, find a little bit of ourselves along the way.
+        """)
+
+st.text(spaces)
+st.divider()
+st.text(spaces)
+
+st.write("Thanks for stopping by, hope you'll enjoy it! 🤖 ")
+st.write("In case you liked it or the long calls - feel free to subscibe to ")
+st.write("🎙️'Interactive Podcast'🎙️")
+# st.write("hosted by Renate and Plamen.")
+# st.write('🐫🐫🐫🐫🐫🐫🐫🐫🐫🐫🐫🐫🐫🐫🐫🐫🐫🐫🐫🐫🐫🐫🐫🐫🐫🐫🐫🐫🐫🐫🐫🐫')
